bcd584bcb3852f77c33f28d58fd6ea4ad3f90d32
Jonathan McDermid <jonathan.mcdermid1@ucalgary.ca>
2020-06-17 02:04:23 -0600

    contact form

---
+++ contactform.php
@@ -0,0 +1,16 @@
+﻿<?php
+
+if(isset($_POST['submit'])){
+	$mailFrom = $_POST['email'];
+	$message = $_POST['message'];
+
+	$mailTo = "jonathan.mcdermid1@ucalgary.ca";
+	$headers = "From: ".$mailFrom;
+	$subject = "A website user wants to talk!";
+	$txt = $message;
+
+	mail($mailTo, $subject, $txt, $headers);
+	header("Location: index.php?mailsend");
+}
+
+?>
