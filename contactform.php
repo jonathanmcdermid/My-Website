14d8177820f810ef0781f57393ff784aa0e17f61
Jonathan McDermid <jonathan.mcdermid1@ucalgary.ca>
2020-06-17 18:50:08 -0600

    more syntax

--- contactform.php
+++ contactform.php
@@ -3,9 +3,10 @@
         $mailFrom = "admin@jon.engineering";
         $mailTo = "jonathan.mcdermid1@ucalgary.ca";
         $txt = $_POST['message'];
-	    $subject = "A user wants to talk! From: " .$POST['email'];
+	    $subject = "A user wants to talk! From: " .$_POST['email'];
         $headers = "From :" .$mailFrom;
         mail ($mailTo, $subject, $txt, $headers);
         header("Location: contact.html");
         echo 'Your message has been sent!';
     }
+?>
