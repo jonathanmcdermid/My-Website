d80ce015c9bc3565b93a8750faf271ace469a4a9
Jonathan McDermid <jonathan.mcdermid1@ucalgary.ca>
2020-06-17 18:47:12 -0600

    semicolon lol

--- contactform.php
+++ contactform.php
@@ -5,7 +5,7 @@
         $txt = $_POST['message'];
 	    $subject = "A user wants to talk! From: " .$POST['email'];
         $headers = "From :" .$mailFrom;
-        mail ($mailTo, $subject, $txt, $headers)
+        mail ($mailTo, $subject, $txt, $headers);
         header("Location: contact.html");
         echo 'Your message has been sent!';
     }
